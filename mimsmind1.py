fde60bcb47e6cb01671e67119fb033d48cab8993
Neeragrover <maverick.neera@gmail.com>
2015-08-17 20:35:23 -0700

    updated mimsmind1

--- mimsmind1.py
+++ mimsmind1.py
@@ -18,11 +18,38 @@ def check_for_num(number):
 		return -1
 	else:
 		return int_num
+
+def evaluate_bulls_cows(guessed_num_raw, str_magic_num,num_of_digits):
+	"""This function evaluates and returns the number of bulls and cows."""
+	guessed_list = list(guessed_num_raw)	#converting the strings to lists to check for bulls
+	magic_list = list(str_magic_num)
+	list_bulls = []
+	cows = 0
+	bulls = 0
+
+# iterating the two lists to find matches and get a count of bulls	
+	for i in range(num_of_digits):
+		if guessed_list[i] == magic_list[i]:
+			bulls += 1
+			list_bulls.append(guessed_list[i])
+			
+#eliminating the bulls to find cows
+	for item in list_bulls:
+		guessed_list.remove(item)
+		magic_list.remove(item)
+
+#iterating the remaining lists to get a count for cows
+	for item in guessed_list:
+		if item in magic_list:
+			cows += 1
+							
+	print bulls,"bull(s)",cows,"cow(s).",
+	return bulls, cows
+	
+		
 		
 def main(): 
 	num_of_tries = 0
-	cows = 0
-	bulls = 0
 	num_of_digits_raw = get_num_of_digits()
 	num_of_digits = check_for_num(num_of_digits_raw)
 	
@@ -36,44 +63,25 @@ def main():
 	
 	magic_num = random.randint(10**(num_of_digits-1), 10**num_of_digits-1)
 	str_magic_num = str(magic_num)
-	print str_magic_num
+#	print str_magic_num
 	
 	print "Guess a",num_of_digits,"digit number (# 1 ):",
 			
 	while (num_of_tries < maxrounds):	
 		num_of_tries += 1
 		
-		#if (num_of_tries == maxrounds+1 and bulls != num_of_digits):
-		#if (num_of_tries == maxrounds):
-		#		print "Sorry. You did not guess the number in",maxrounds,"tries.The correct number is",magic_num
-		#		exit(0)
-		#elif num_of_tries == 1:
-		#	print "Guess a",num_of_digits,"digit number :",
-		#else:
 		if(num_of_tries > 1):
 			print "Try again (#",num_of_tries,"):",
 				
 		guessed_num_raw = raw_input()
 		guessed_num = check_for_num(guessed_num_raw)
-		#print guessed_num
+
 		
 		if ((guessed_num == -1) or (len(guessed_num_raw) != num_of_digits)):
 			print "Invalid input.",
 			continue
 		else:
-			for i in range(num_of_digits):
-				if guessed_num_raw[i] == str_magic_num[i]:
-					bulls += 1
-				else:
-					for i in guessed_num_raw:
-						if str_magic_num.find(i) != -1:
-							cows += 1
-							
-			cows = cows - bulls
-			if cows < 0:
-				cows = 0
-
-		print bulls,"bull(s)",cows,"cow(s).",
+			bulls, cows = evaluate_bulls_cows(guessed_num_raw, str_magic_num,num_of_digits)
 		
 		if bulls != num_of_digits:
 			bulls = 0
